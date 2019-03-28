55c0c8d2f6b01ff2f37fa82360ce0473b699d065
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-28 17:43:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,7 +13,7 @@ def key_for_min_value(name_hash)
       min_key = key
     end
   end
-  key
+  min_key
 end
 
 #This method should iterate over the hash and return the key (not the value!) that points to the smallest value of the set. If the method is called and passed an argument of an empty hash, it should return nil.
