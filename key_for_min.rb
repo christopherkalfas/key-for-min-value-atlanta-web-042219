256e51e185a6b2b7ae70ecccb7c5c1b9c833037e
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-28 17:43:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_key =nil 
   min_value = nil 
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
     if min_key == nil
       min_value = value
       min_key = key 
