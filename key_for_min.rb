d9142f245d4b550df22cf500122e7a6a402e6e9b
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-28 17:44:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
     if min_value == nil
       min_value = value
       min_key = key 
-    else min_value > value
+    elsif min_value > value
       min_value = value
       min_key = key
     end
